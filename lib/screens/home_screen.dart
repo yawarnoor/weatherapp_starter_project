780eb465ab445d59e105c229b490d43788021a94
yawarnoor <yawarnoor.bscssef20@iba-suk.edu.pk>
2023-08-07 02:36:45 +0500

    Weather app inception

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/global_controller.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
